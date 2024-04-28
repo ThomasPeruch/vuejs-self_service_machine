93d237d430337002876867542d6b758d48549419
Thomas Peruch <peruchthomas@gmail.com>
2024-04-28 17:26:18 -0300

    highlighting selected product with style binding(:class) and event handling(v-on)

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 var products = [
     {
         "photo": "img/big-mac.png",
-        "name": "Big méqui",
+        "name": "Big Mac",
         "price": 5.99,
         "active": false,
         "quantity": 1
